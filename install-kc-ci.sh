3558d41101511f672cfae57df1dace5701ed6752
Ruben Taelman <ruben.taelman@ugent.be>
2018-10-30 07:21:31 +0000

    Take into account redirects when installing KC

--- install-kc-ci.sh
+++ install-kc-ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /opt
-curl -O http://fallabs.com/kyotocabinet/pkg/kyotocabinet-1.2.76.tar.gz
+curl -LO http://fallabs.com/kyotocabinet/pkg/kyotocabinet-1.2.76.tar.gz
 tar -xvzf kyotocabinet-1.2.76.tar.gz && mv kyotocabinet-1.2.76 kyotocabinet && rm kyotocabinet-1.2.76.tar.gz
 
 apt-get update
