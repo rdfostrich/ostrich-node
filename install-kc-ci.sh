97dbba1516faf2f84d064a2307ffb0da3165d200
Olivier Pelgrin <olivier@cs.aau.dk>
2022-07-08 14:02:08 +0200

    update install kyotocabinet script to newer version

--- install-kc-ci.sh
+++ install-kc-ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /opt
-curl -LO http://fallabs.com/kyotocabinet/pkg/kyotocabinet-1.2.76.tar.gz
-tar -xvzf kyotocabinet-1.2.76.tar.gz && mv kyotocabinet-1.2.76 kyotocabinet && rm kyotocabinet-1.2.76.tar.gz
+curl -LO https://dbmx.net/kyotocabinet/pkg/kyotocabinet-1.2.79.tar.gz
+tar -xvzf kyotocabinet-1.2.79.tar.gz && mv kyotocabinet-1.2.79 kyotocabinet && rm kyotocabinet-1.2.79.tar.gz
 
 apt-get update
 apt-get -y install liblzo2-dev liblzma-dev zlib1g-dev build-essential
-cd /opt/kyotocabinet && ./configure –enable-zlib –enable-lzo –enable-lzma && make && make install
+cd /opt/kyotocabinet && ./configure --enable-lzo --enable-lzma && make && make install
