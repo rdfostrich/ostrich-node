060b6230e43a6336544d854dd60f95a4499484ae
Ruben Taelman <ruben.taelman@ugent.be>
2022-10-13 06:50:25 +0200

    Fix failing GH actions builds

--- install-kc-ci.sh
+++ install-kc-ci.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-if [ ! -d "/opt/kyotocabinet" ]; then
-  cd /opt
-  curl -LO https://dbmx.net/kyotocabinet/pkg/kyotocabinet-1.2.79.tar.gz
-  tar -xvzf kyotocabinet-1.2.79.tar.gz && mv kyotocabinet-1.2.79 kyotocabinet && rm kyotocabinet-1.2.79.tar.gz
+cd /opt
+curl -LO https://dbmx.net/kyotocabinet/pkg/kyotocabinet-1.2.79.tar.gz
+tar -xvzf kyotocabinet-1.2.79.tar.gz && mv kyotocabinet-1.2.79 kyotocabinet && rm kyotocabinet-1.2.79.tar.gz
 
-  apt-get update
-  apt-get -y install liblzo2-dev liblzma-dev zlib1g-dev build-essential
-  cd /opt/kyotocabinet && ./configure --enable-lzo --enable-lzma && make && make install
-fi
+apt-get update
+apt-get -y install liblzo2-dev liblzma-dev zlib1g-dev build-essential
+cd /opt/kyotocabinet && ./configure --enable-lzo --enable-lzma && make && make install
